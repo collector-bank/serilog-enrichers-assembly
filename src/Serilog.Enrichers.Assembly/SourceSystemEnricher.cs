e21faca03663ba7527d5ecd7638d831b1db9a7ab
Adam Lith <adam.lith@gmail.com>
2016-11-23 14:05:33 +0100

    Created SourceSystemEnricher.

---
+++ src/Serilog.Enrichers.Assembly/SourceSystemEnricher.cs
@@ -0,0 +1,43 @@
+﻿namespace Serilog.Enrichers.Assembly
+{
+    using System;
+    using System.Reflection;
+
+    using Serilog.Core;
+    using Serilog.Events;
+
+    public class SourceSystemEnricher<T> : SourceSystemEnricher
+    {
+        public SourceSystemEnricher()
+            : base(typeof(T).Assembly)
+        {
+        }
+    }
+
+    public class SourceSystemEnricher : ILogEventEnricher
+    {
+        private readonly string _name;
+
+        public SourceSystemEnricher(string name)
+        {
+            _name = name;
+        }
+
+        public SourceSystemEnricher(Assembly assembly)
+        {
+            if (assembly == null)
+                throw new ArgumentNullException(nameof(assembly));
+
+            _name = assembly.GetName().Name;
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (string.IsNullOrWhiteSpace(_name))
+                return;
+
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("SourceSystem", _name));
+
+        }
+    }
+}
---
+++ test/Serilog.Enrichers.Assembly.UnitTest/SourceSystemEnricher_Test.cs
@@ -0,0 +1,52 @@
+﻿namespace Serilog.Enrichers.Assembly.UnitTest
+{
+    using NUnit.Framework;
+
+    using Serilog.Events;
+
+    [TestFixture]
+    public class SourceSystemEnricher_Test
+    {
+        [Test]
+        public void When_using_the_generic_version_the_assembly_name_of_the_type_is_used()
+        {
+            LogEvent logEvent = null;
+            var logger = new LoggerConfiguration()
+                .Enrich.With<SourceSystemEnricher<SourceSystemEnricher_Test>>()
+                .WriteTo.Sink(new DelegatingSink(e => logEvent = e))
+                .CreateLogger();
+
+            logger.Information("Test log");
+
+            Assert.AreEqual("Serilog.Enrichers.Assembly.UnitTest", ((ScalarValue)logEvent.Properties["SourceSystem"]).Value);
+        }
+
+        [Test]
+        public void It_takes_the_assembly_name_from_the_supplied_assembly()
+        {
+            LogEvent logEvent = null;
+            var logger = new LoggerConfiguration()
+                .Enrich.With(new SourceSystemEnricher(typeof(Serilog.Context.LogContext).Assembly))
+                .WriteTo.Sink(new DelegatingSink(e => logEvent = e))
+                .CreateLogger();
+
+            logger.Information("Test log");
+
+            Assert.AreEqual("Serilog", ((ScalarValue)logEvent.Properties["SourceSystem"]).Value);
+        }
+
+        [Test]
+        public void It_uses_the_supplied_name_if_sent_as_string()
+        {
+            LogEvent logEvent = null;
+            var logger = new LoggerConfiguration()
+                .Enrich.With(new SourceSystemEnricher("My String"))
+                .WriteTo.Sink(new DelegatingSink(e => logEvent = e))
+                .CreateLogger();
+
+            logger.Information("Test log");
+
+            Assert.AreEqual("My String", ((ScalarValue)logEvent.Properties["SourceSystem"]).Value);
+        }
+    }
+}
